1e9c5159e6cc0e9a904c44e47a1f14780527f155
VictorMedel99 <victor.medelito@gmail.com>
2020-11-09 21:55:10 -0600

    se modifico la navegacion entre ventanas

--- src/java/bean/indexBeanD.java
+++ src/java/bean/indexBeanD.java
@@ -1,56 +1,58 @@
-package bean;
-
-import departamentos.*;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-
-@ManagedBean
-@RequestScoped
-public class indexBeanD extends Departamento{
-    
-    private Departamento departamento;
-    private DepartamentoDAO departamentoDao;
-
-    public indexBeanD(String id, String departamento) {
-        super(id, departamento);
-    }
-    
-    public indexBeanD() {
-        this.departamentoDao = new DepartamentoDAO();
-    }
-
-    @Override
-    public void setId(String id) {
-        super.setId(id); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void setDepartamento(String departamento) {
-        super.setDepartamento(departamento); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    public void insert() {
-        departamento = new Departamento(getId(), getDepartamento());
-        departamentoDao.insert(departamento);
-        
-    }
-
-    public void update() {
-        departamento = new Departamento(getId(), getDepartamento());
-        departamentoDao.update(departamento);
-    }
-
-    public void delete() {
-        departamento = new Departamento(getId(), getDepartamento());
-        departamentoDao.delete(Integer.parseInt(departamento.getId()));
-    }
-
-    public void find(Integer id) {
- 
-    }
-
-    public void findAll() {
-        
-    }
-    
-}
+//package bean;
+//
+//import services.Crud;
+//import services.Crud_Service;
+//import javax.faces.bean.ManagedBean;
+//import javax.faces.bean.RequestScoped;
+//
+//@ManagedBean
+//@RequestScoped
+//public class indexBeanD extends Departamento{
+//    
+//    private services.Departamento dep;
+//    private Crud_Service service;
+//    private Crud interfaz;
+//
+//    public indexBeanD(String id, String departamento) {
+//        super(id, departamento);
+//    }
+//    
+//    public indexBeanD() {
+//        this.departamentoDao = new DepartamentoDAO();
+//    }
+//
+//    @Override
+//    public void setId(String id) {
+//        super.setId(id); //To change body of generated methods, choose Tools | Templates.
+//    }
+//
+//    @Override
+//    public void setDepartamento(String departamento) {
+//        super.setDepartamento(departamento); //To change body of generated methods, choose Tools | Templates.
+//    }
+//    
+//    public void insert() {
+//        departamento = new Departamento(getId(), getDepartamento());
+//        departamentoDao.insert(departamento);
+//        
+//    }
+//
+//    public void update() {
+//        departamento = new Departamento(getId(), getDepartamento());
+//        departamentoDao.update(departamento);
+//    }
+//
+//    public void delete() {
+//        departamento = new Departamento(getId(), getDepartamento());
+//        departamentoDao.delete(Integer.parseInt(departamento.getId()));
+//    }
+//
+//    public void find(Integer id) {
+// 
+//    }
+//
+//    public void findAll() {
+//        
+//    }
+//    
+//}
--- src/java/bean/indexBeanE.java
+++ src/java/bean/indexBeanE.java
@@ -1,66 +1,71 @@
-package bean;
-
-import empleados.*;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-
-@ManagedBean
-@RequestScoped
-public class indexBeanE extends Empleado {
-
-    private Empleado empleado;
-    private EmpleadoDAO empleadoDao;
-
-    public indexBeanE(String id, String nombre, String direccion, String telefono) {
-        super(id, nombre, direccion, telefono);
-    }
-    
-    public indexBeanE() {
-        this.empleadoDao = new EmpleadoDAO();
-    }
-
-    @Override
-    public void setId(String id) {
-        super.setId(id); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void setNombre(String nombre) {
-        super.setNombre(nombre); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void setDireccion(String direccion) {
-        super.setDireccion(direccion); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void setTelefono(String telefono) {
-        super.setTelefono(telefono); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    public void insert() {
-        empleado = new Empleado(getId(), getNombre(), getDireccion(), getTelefono());
-        empleadoDao.insert(empleado);
-        
-    }
-
-    public void update() {
-        empleado = new Empleado(getId(), getNombre(), getDireccion(), getTelefono());
-        empleadoDao.update(empleado);
-    }
-
-    public void delete() {
-        empleado = new Empleado(getId(), getNombre(), getDireccion(), getTelefono());
-        empleadoDao.delete(Integer.parseInt(empleado.getId()));
-    }
-
-    public void find(Integer id) {
- 
-    }
-
-    public void findAll() {
-        
-    }
-    
-}
+//package bean;
+//
+//import services.Crud;
+//import services.Crud_Service;
+//import javax.faces.bean.ManagedBean;
+//import javax.faces.bean.RequestScoped;
+//
+//@ManagedBean
+//@RequestScoped
+//public class indexBeanE extends Empleado {
+//
+//    private services.Empleado emp;
+//    private Crud_Service service;
+//    private Crud interfaz;
+//
+//    public indexBeanE(String id, String nombre, String direccion, String telefono) {
+//        super(id, nombre, direccion, telefono);
+//    }
+//    
+//    public indexBeanE() {
+//        service = new Crud_Service();
+//        interfaz = service.getCrudPort();
+//    }
+//
+//    @Override
+//    public void setId(String id) {
+//        emp.setID(id);
+//        super.setId(id); //To change body of generated methods, choose Tools | Templates.
+//    }
+//
+//    @Override
+//    public void setNombre(String nombre) {
+//        super.setNombre(nombre); //To change body of generated methods, choose Tools | Templates.
+//    }
+//
+//    @Override
+//    public void setDireccion(String direccion) {
+//        super.setDireccion(direccion); //To change body of generated methods, choose Tools | Templates.
+//    }
+//
+//    @Override
+//    public void setTelefono(String telefono) {
+//        super.setTelefono(telefono); //To change body of generated methods, choose Tools | Templates.
+//    }
+//    
+//    public void insert() {
+//        emp = new services.Empleado();
+//        empleado = new Empleado(getId(), getNombre(), getDireccion(), getTelefono());
+//        empleadoDao.insert(empleado);
+//        
+//    }
+//
+//    public void update() {
+//        empleado = new Empleado(getId(), getNombre(), getDireccion(), getTelefono());
+//        empleadoDao.update(empleado);
+//    }
+//
+//    public void delete() {
+//        empleado = new Empleado(getId(), getNombre(), getDireccion(), getTelefono());
+//        empleadoDao.delete(Integer.parseInt(empleado.getId()));
+//    }
+//
+//    public void find(Integer id) {
+// 
+//    }
+//
+//    public void findAll() {
+//        
+//    }
+//    
+//}
